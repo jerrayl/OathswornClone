430119ccaf9979e1eee234efe87025fbc567d3f4
Jerrayl <jerraylng@gmail.com>
2023-10-17 20:40:28 +1100

    Add class icons

---
+++ ReactFrontend/src/assets/icons/ClassIcons.ts
@@ -0,0 +1,28 @@
+import blade from "./Blade.png";
+import cur from "./Cur.png";
+import exile from "./Exile.png";
+import grovemaiden from "./GroveMaiden.png";
+import harbinger from "./Harbinger.png";
+import huntress from "./Huntress.png";
+import penitent from "./Penitent.png";
+import priest from "./Priest.png";
+import ranger from "./Ranger.png";
+import warbear from "./Warbear.png";
+import warden from "./Warden.png";
+import witch from "./Witch.png";
+
+export const iconMap : {[key: string]: string} = {
+    "Blade" : blade,
+    "Cur": cur,
+    "Exile": exile,    
+    "Grove Maiden": grovemaiden,
+    "Harbinger": harbinger,
+    "Huntress": huntress,    
+    "Penitent" : penitent,
+    "Priest": priest,
+    "Ranger": ranger,    
+    "Warbear": warbear,
+    "Warden": warden,
+    "Witch": witch
+}
+
