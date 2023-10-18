4887c11d13d35c890cb51aea48a033cfd72cc24d
Jerrayl <jerraylng@gmail.com>
2023-10-18 23:11:03 +1100

    hack board and add coordinate system

--- ReactFrontend/src/components/Game.tsx
+++ ReactFrontend/src/components/Game.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { AttackModal } from "./AttackModal";
 import { Button } from "./shared/Button";
+import { BOARD_MAPPING } from "../utils/constants";
 
-export const BOARD = Array(161).fill([]);
+export const BOARD = Array(161 + 10).fill([]);
 
 export const Game = () => {
   const [showModal, setShowModal] = useState(false);
@@ -10,17 +11,22 @@ export const Game = () => {
   return (
     <div>
       {showModal && <AttackModal closeModal={() => setShowModal(false)} />}
-      <div className="flex justify-evenly mt-2">
-        <Button text="Attack" onClick={() => setShowModal(true)} />
-      </div>
-      <div className="main flex">
-        <div className="container">
-          {
-            BOARD.map(x => <div />)
-          }
+      <div className="grid grid-cols-6">
+        <div className="flex justify-evenly col-span-1 mt-2">
+          <div>
+            <Button text="Attack" onClick={() => setShowModal(true)} />
+          </div>
+        </div>
+        <div className="main flex mt-6 col-span-4">
+          <div className="container">
+            {
+              BOARD.map((x, i) => <div key={i} className="caret-transparent flex justify-center align-center pl-10 pt-6 text-2xl">{BOARD_MAPPING[i] !== "x" ? String(BOARD_MAPPING[i]) : i}</div>)
+            }
+          </div>
+        </div>
+        <div className="col-span-1">
         </div>
       </div>
-
     </div>
   );
 };
